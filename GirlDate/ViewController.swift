9c26ae58fe0eb31b1a29093c66ce55255e16fa4b
One <appdevoney@gmail.com>
2015-01-15 00:01:34 +0800

    Initial Commit

---
+++ GirlDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GirlDate
+//
+//  Created by YANG WAN-HUANG on 2015/1/15.
+//  Copyright (c) 2015年 ithink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
